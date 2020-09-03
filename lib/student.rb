a52838a1e239222c70dabe48a207943efd6e7aa6
MullaN <admullan@gmail.com>
2020-09-02 17:15:50 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,51 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    self.many_new_from_db(DB[:conn].execute("SELECT * FROM students"))
+  end
+
+  def self.all_students_in_grade_9
+    self.all_students_in_grade_X(9)
+  end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12"
+    self.many_new_from_db(DB[:conn].execute(sql))
+  end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    self.many_new_from_db(DB[:conn].execute(sql, grade))
+  end
+
+  def self.first_X_students_in_grade_10(num_students)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    self.many_new_from_db(DB[:conn].execute(sql, num_students))
+  end
+
+  def self.first_student_in_grade_10
+    self.all_students_in_grade_X(10).first
+  end
+
+  def self.many_new_from_db(many_rows)
+    many_rows.map {|student| self.new_from_db(student)}
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = "SELECT * FROM students WHERE name = ?"
+    row = DB[:conn].execute(sql, name).first
+    self.new_from_db(row)
   end
   
   def save
